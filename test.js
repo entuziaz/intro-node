13f0673c5a3f422e24ec736e92b9c9dad498a9b0
jkaylight <jkaylight@gmail.com>
2025-11-09 00:18:00 +0100

    fs module

---
+++ test.js
@@ -0,0 +1,15 @@
+import { write } from 'node:fs'
+import fs from 'node:fs/promises'
+
+const readPjson = async() => {
+    const pjsonPath = new URL('./package.json', import.meta.url).pathname
+    console.log(JSON.parse(await fs.readFile(pjsonPath, 'utf-8')))
+}
+
+const writeFile = async () => {
+    const newFile =  new URL('./demo.js', import.meta.url).pathname 
+    await fs.writeFile(newFile, `console.log('yoooo!')`)
+}
+
+// readPjson()
+writeFile()
