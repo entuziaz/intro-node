139b32186b6cb79367d5fd4fef22fa41468a7510
jkaylight <jkaylight@gmail.com>
2025-11-09 22:30:05 +0100

    list all notes command

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+const listNotes = notes => {
+  notes.forEach(({id, content, tags}) => {
+    console.log('id: ', id)
+    console.log('tags: ', tags)
+    console.log('content: ', content)
+    console.log('\n')
+  })
+}
+
+export default listNotes;
